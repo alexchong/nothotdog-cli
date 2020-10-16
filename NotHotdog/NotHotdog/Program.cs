1cffb5788335fbe24cd6c0ae9e24998a2037c56e
Alex Chong <alexbchong@pm.me>
2020-10-15 23:56:28 -0700

    Implement AnalyzeImage method to return image data via url

--- NotHotdog/NotHotdog/Program.cs
+++ NotHotdog/NotHotdog/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 
@@ -20,35 +22,22 @@ namespace NotHotdog
         /// </summary>
         public void ReadSubscriptionKey()
         {
-            {
-                try
-                {
-                    // TODO: Retrieve path  based on user directory/environment variables
-                    string filePath = @"C:\Users\Alex\source\repos\NotHotdog\NotHotdog\key.txt";
 
-                    using (StreamReader sr = File.OpenText(filePath))
-                    {
-                        string line = "";
-                        int count = 0;
-                        while ((line = sr.ReadLine()) != null && count < 2)
-                        {
-                            SubscriptionKey[count] = line;
-                            count++;
-                        }
-                    }
-                }
-                catch (IOException e)
+            // TODO: Retrieve path  based on user directory/environment variables
+            string filePath = @"C:\Users\Alex\source\repos\NotHotdog\NotHotdog\key.txt";
+
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                string line = "";
+                int count = 0;
+                while ((line = sr.ReadLine()) != null && count < 2)
                 {
-                    Console.WriteLine(e.Message);
-                }
-                // TODO: Implement more specific Exceptions for read file errors
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
+                    SubscriptionKey[count] = line;
+                    count++;
                 }
             }
-        }
 
+        }
 
         /// <summary>
         /// Authenticate API key to create new Computer Vision client
@@ -59,8 +48,44 @@ namespace NotHotdog
         public ComputerVisionClient Authenticate(string key, string endpoint)
         {
             ComputerVisionClient client = new ComputerVisionClient
-                (new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+                (new ApiKeyServiceClientCredentials(key))
+            { Endpoint = endpoint };
             return client;
+        }
+
+        public async Task AnalyzeImage(ComputerVisionClient client)
+        {
+            while (true)
+            {
+
+                Console.Write("Enter image url: ");
+
+                // Use regex to test for valid image url (e.g. ...com/hot-dog.jpg)
+                var url = Console.ReadLine();
+
+                // TODO: If image is not hotdog, let the user know the image is literally not a hotdog; 
+                List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+                    {
+                        VisualFeatureTypes.Description, VisualFeatureTypes.Categories
+                    };
+
+                ImageAnalysis results = await client.AnalyzeImageAsync(url, features);
+
+                // NOTE: Testing get methods for image captions
+                foreach (var caption in results.Description.Captions)
+                {
+                    Console.WriteLine($"{caption.Text} with confidence {caption.Confidence}");
+                }
+
+                foreach (var caption in results.Categories)
+                {
+                    Console.WriteLine($"{caption.Name} with confidence {caption.Score}");
+                }
+
+                return;
+
+            }
+
         }
 
         public NotHotdog()
@@ -69,6 +94,7 @@ namespace NotHotdog
             {
                 ReadSubscriptionKey();
                 ComputerVisionClient client = Authenticate(SubscriptionKey[0], SubscriptionKey[1]);
+                AnalyzeImage(client).Wait();
             }
             // Catch any unexpected errors
             catch (Exception e)
@@ -81,7 +107,7 @@ namespace NotHotdog
     {
         static void Main(string[] args)
         {
-
+            NotHotdog app = new NotHotdog();
         }
     }
 }
